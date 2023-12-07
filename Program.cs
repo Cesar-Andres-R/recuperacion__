497e2550abedcae15a5cb965e13f441b09e8032a
Cesar-Andres-R <bocchi.the.rock.010@gmail.com>
2023-12-07 10:35:20 -0700

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using recuperacion_.metodos;
+
+namespace recuperacion_
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Examen ex = new Examen();
+
+            ex.Ingresar();
+
+        }
+    }
+}
---
+++ metodos/Examen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace recuperacion_.metodos
+{
+    public class Examen
+    {
+        string[] productos = new string[10];
+
+
+        public void Ingresar()
+        {
+            Console.WriteLine("ingrese prodecto");
+            for (int i = 0; i < 4; i++)
+            {
+                string product = Console.ReadLine();
+                int precio = int.Parse(Console.ReadLine());
+            }
+        }
+
+    }
+}
